b8fc0fbf57539182c872b0bec4b1c433d49f50c4
divvy96 <divvy96@gmail.com>
2025-02-14 00:10:03 +0000

    historical mix by postcode

--- carbon_intensity_data.py
+++ carbon_intensity_data.py
@@ -3,6 +3,34 @@ import requests
 
 import pandas
 
+def get_historical_postcode_mix(start_datetime: datetime.datetime, postcode: str):
+
+    period_start = start_datetime.isoformat()
+    url = f'https://api.carbonintensity.org.uk/regional/intensity/{period_start}/fw48h/postcode/{postcode}'
+    response = requests.get(url)
+    if response.status_code != 200:
+        print("Error retreiving data")
+        return
+
+    data = response.json()
+    generation_mix_history = []
+    for start_time in data['data']['data']:
+        for generation_values in start_time['generationmix']:
+            generation_mix_history.append(
+                    (generation_values['fuel'],
+                     generation_values['perc'],
+                     start_time['from'],
+                     start_time['to'],
+                     postcode))
+
+    df = pandas.DataFrame(generation_mix_history,
+                          columns = ['fuel',
+                                     'percentage',
+                                     'from',
+                                     'to'])
+    df['from'] = pandas.to_datetime(df['from'])
+    df['to'] = pandas.to_datetime(df['to'])
+    return df
 
 def get_current_intensity():
     response = requests.get(r"https://api.carbonintensity.org.uk/intensity")
